4bc9dc41d05c1d57b1c71f25a646ff6a70ed59e5
aaryan19 <a.rnsesta@gmail.com>
2025-03-13 14:03:00 +0545

    comment change

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -11,7 +11,7 @@ import Footer from '@/components/Footer';
 
 const Index = () => {
   useEffect(() => {
-    // Function to handle scroll animations
+    // handles scroll animations
     const handleScrollAnimations = () => {
       const animatedElements = document.querySelectorAll('.fade-in, .slide-up, .slide-in-right, .scale-in');
       
