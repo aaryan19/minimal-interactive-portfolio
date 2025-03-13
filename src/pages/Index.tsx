117ea6e68388ac496c2edb7f54e6dc81f55037da
aaryan19 <a.rnsesta@gmail.com>
2025-03-13 14:57:47 +0545

    test3

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -11,7 +11,7 @@ import Footer from '@/components/Footer';
 
 const Index = () => {
   useEffect(() => {
-    // handles scroll animations pheri
+    // handles scroll animations
     const handleScrollAnimations = () => {
       const animatedElements = document.querySelectorAll('.fade-in, .slide-up, .slide-in-right, .scale-in');
       
